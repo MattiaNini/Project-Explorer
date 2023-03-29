e67fe739db64fdc49e2a690b5d146c92de50edf2
MattiaNini <121028609+MattiaNini@users.noreply.github.com>
2023-03-29 17:46:15 +0200

    Project-Explorer

---
+++ project-explorer/src/main.jsx
@@ -0,0 +1,40 @@
+import "./index.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import MainLayout from "./layouts/mainLayout";
+import Activities from "./pages/activities";
+import Activity from "./pages/activities/id";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+
+//-- ROUTER-- //
+
+//Home & About = Singole
+//Activiy & City = Dinamiche
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<MainLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="activities" element={<Activities />} />
+      <Route path="activities/:id" element={<Activity />} />
+      <Route path="city/:id" element={<h1>Città</h1>} />
+    </Route>
+  )
+);
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ project-explorer/src/pages/About.jsx
@@ -0,0 +1,10 @@
+import { Link, Outlet } from "react-router-dom";
+import styles from "../styles/About.module.scss";
+
+export default function About() {
+  return (
+    <div className={styles.About}>
+      <h1>ABOUT</h1>
+    </div>
+  );
+}
